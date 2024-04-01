203c08ad51800291f23c1617cb3f48708f489b53
NikolaiShinkorenko <sinkorenko06@mail.ru>
2024-04-01 22:57:26 +0600

    Race type selection keyboard

--- bot.py
+++ bot.py
@@ -5,9 +5,11 @@ import logging
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.fsm.context import FSMContext
+
+RACE_LIST = ["Полумарафон", "Марафон", "Сверхмарафон"]
 
 class Reg(StatesGroup):
     name = State()
@@ -17,9 +19,18 @@ load_dotenv()
 bot = Bot(os.getenv('TOKEN'))
 dp = Dispatcher()
 
+race_types = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text=RACE_LIST[0])],
+    [KeyboardButton(text=RACE_LIST[1])],
+    [KeyboardButton(text=RACE_LIST[2])]],
+    resize_keyboard=True,
+    one_time_keyboard=True,
+    input_field_placeholder="Выберете пункт меню")
+
 @dp.message(CommandStart())
 async def start(message: Message):
-    await message.answer("Здравствуйте! Этот бот предназначен для записи на марафон.")
+    await message.answer("Здравствуйте! Этот бот предназначен для записи на марафон.\n"
+                         "Чтобы зарегистрироваться на забег используйте команду /reg.")
 
 @dp.message(Command('reg'))
 async def reg_first(message: Message, state: FSMContext):
@@ -30,14 +41,18 @@ async def reg_first(message: Message, state: FSMContext):
 async def reg_second(message: Message, state: FSMContext):
     await state.update_data(name=message.text)
     await state.set_state(Reg.race_type)
-    await message.answer("Выберете тип забега (полумарафон, марафон, сверхмарафон)")
+    await message.answer("Выберете тип забега",
+                         reply_markup=race_types)
 
 @dp.message(Reg.race_type)
 async def reg_third(message: Message, state: FSMContext):
-    await state.update_data(race_type=message.text)
-    data = await state.get_data()
-    await message.answer(f'Поздравляем, {data["name"]}, вы успешно записались на {data["race_type"]}!')
-    await state.clear()
+    if message.text in RACE_LIST:
+        await state.update_data(race_type=message.text)
+        data = await state.get_data()
+        await message.answer(f'Поздравляем, {data["name"]}, вы успешно записались на {data["race_type"].lower()}!')
+        await state.clear()
+    else:
+        await message.answer("Ошибка, такого варианта нет в моем списке 🥺")
 
 async def main():
     await dp.start_polling(bot)
